ce80844f342d49c93df448b8d1efa0c8e501466f
rbleng <rbleng@foxmai.com>
2018-03-24 00:30:02 +0800

    2 files

--- src/main/java/org/seckill/dao/SeckillDao.java
+++ src/main/java/org/seckill/dao/SeckillDao.java
@@ -1,16 +1,16 @@
 package org.seckill.dao;
 
-import org.seckill.entity.Seckill;
 import org.apache.ibatis.annotations.Param;
+import org.seckill.entity.Seckill;
 
 import java.util.Date;
 import java.util.List;
 
-public interface SeckillDao
-{
+public interface SeckillDao {
 
     /**
      * 减库存
+     *
      * @param seckillId
      * @param killTime
      * @return 如果影响行数>1，表示更新库存的记录行数
@@ -19,6 +19,7 @@ public interface SeckillDao
 
     /**
      * 根据id查询秒杀的商品信息
+     *
      * @param seckillId
      * @return
      */
@@ -26,12 +27,12 @@ public interface SeckillDao
 
     /**
      * 根据偏移量查询秒杀商品列表
+     *
      * @param offset
      * @param limit
      * @return
      */
-    List<Seckill> queryAll(@Param("offset") int offset,@Param("limit") int limit);
-
+    List<Seckill> queryAll(@Param("offset") int offset, @Param("limit") int limit);
 
 
 }
